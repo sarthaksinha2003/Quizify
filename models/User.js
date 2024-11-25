1965e762ef45f6fe868211d8f43890ac965effcf
Sarthak Sinha <167212360+sarthaksinha2003@users.noreply.github.com>
2024-11-25 15:27:54 +0530

    Project Completed

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true } // This will store the hashed password
+    password: { type: String, required: true }
 });
 
 const User = mongoose.model('User', UserSchema);
